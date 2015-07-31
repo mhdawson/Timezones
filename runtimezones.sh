ba34ab3e18a9ae2373d5998b9261e88919e3ba7c
Michael Dawson <mdawson@devrus.com>
2015-07-31 02:04:49 +0000

    Format minutes so that we have leading 0
    Format minutes that we always have 2 digits
    by adding leadning 0 when necessary

--- runtimezones.sh
+++ runtimezones.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export PATH=/home/node/timezones/node-v0.12.5-linux-x64/bin:$PATH
-cd /home/node/timezones
+export PATH=/home/node/Timezones/node-v0.12.5-linux-x64/bin:$PATH
+cd /home/node/Timezones
 node server.js config.txt
